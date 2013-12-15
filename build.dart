ffce355e844222ec31fb20a22d252157c0a79702
Dylan Powers <dylan.kyle.powers@gmail.com>
2013-12-14 18:08:32 -0800

    index.html apparently HAS to be in the web directory for building to work.

--- build.dart
+++ build.dart
@@ -2,5 +2,5 @@ import 'package:polymer/builder.dart';
 
 void main(List<String> args) {
   // Runs the linter, and optionally builds the project for deployment.
-  lint(entryPoints: ['index.html'], options: parseOptions(args));
+  lint(entryPoints: ['web/index.html'], options: parseOptions(args));
 }
