75323e1ba1e25d0c3aa016a6d4b77dd435560efd
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-10-07 01:23:11 -0700

    Moved the header files around a bit

---
+++ web/header/intro_header.dart
@@ -0,0 +1,8 @@
+import 'dart:html';
+import 'package:polymer/polymer.dart';
+
+
+@CustomTag('intro-header')
+class IntroHeaderElement extends PolymerElement {
+  IntroHeaderElement.created() : super.created();
+}
