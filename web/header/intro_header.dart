2525f4ba42be505e9f85bd72c06804094401fdec
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-10-07 23:11:59 -0700

    Dinking around

--- web/header/intro_header.dart
+++ web/header/intro_header.dart
@@ -1,8 +1,15 @@
+import 'dart:async';
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 
 
 @CustomTag('intro-header')
 class IntroHeaderElement extends PolymerElement {
-  IntroHeaderElement.created() : super.created();
+  Element panelEl;
+  StreamSubscription<Event> scrollHandler;
+  
+  IntroHeaderElement.created() : super.created() {
+    panelEl = document.getElementById('panel');
+    
+  }
 }
