b867bd0512cc5b407b32a0250c0ee5188cfc355d
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-10-08 13:50:13 -0700

    Remember those noscripts...they're critical

--- web/header/intro_header.dart
+++ web/header/intro_header.dart
@@ -2,14 +2,24 @@ import 'dart:async';
 import 'dart:html';
 import 'package:polymer/polymer.dart';
 
-
 @CustomTag('intro-header')
 class IntroHeaderElement extends PolymerElement {
-  Element panelEl;
-  StreamSubscription<Event> scrollHandler;
+  Element _panelEl;
+  StreamSubscription<Event> _scrollHandler;
   
   IntroHeaderElement.created() : super.created() {
-    panelEl = document.getElementById('panel');
-    
+    _panelEl = shadowRoot.getElementById('panel');
+  }
+
+  @override
+  void attached() {
+    String standardHeight = _panelEl.style.height;
+    _scrollHandler = window.onScroll.listen((_) {
+      if (window.pageYOffset > 25) {
+        _panelEl.style.height = "36px";
+      } else {
+        _panelEl.style.height = standardHeight;
+      }
+    });
   }
 }
