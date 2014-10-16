55a386d6217e81bac8ab1e8234dd94a740d1e0a2
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-10-15 18:41:42 -0700

    Cool header animation

--- web/header/intro_header.dart
+++ web/header/intro_header.dart
@@ -4,19 +4,57 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('intro-header')
 class IntroHeaderElement extends PolymerElement {
-  @observable String panelStyle = 'panel-expanded';
+  static const String _EXPANDED = 'panel-expanded';
+  static const String _COLLAPSED = 'panel-collapsed';
+
+  @observable String panelStyle = _EXPANDED;
   StreamSubscription<Event> _scrollHandler;
-  
+
+  int _minHeight;
+  int _maxHeight;
+  Element _panel;
+
   IntroHeaderElement.created() : super.created();
 
   @override
   void attached() {
-    _scrollHandler = window.onScroll.listen((_) {
-      if (window.pageYOffset > 36) {
-        panelStyle = 'panel-collapsed';
-      } else {
-        panelStyle = 'panel-expanded';
-      }
+    _panel = shadowRoot.querySelector('#panel');
+    window.onResize.listen((_) {
+      _fullReset();
     });
+
+    _scrollHandler = window.onScroll.listen((_) {
+      _updatePanel();
+    });
+
+    _fullReset();
+  }
+
+  void _fullReset() {
+    var oldClassName = _panel.className;
+    var oldHeight = _panel.style.height;
+
+    _panel.style.height = '';
+    _panel.className = _COLLAPSED;
+    _minHeight = _panel.clientHeight;
+    _panel.className = _EXPANDED;
+    _maxHeight = _panel.clientHeight;
+
+    _panel.className = oldClassName;
+    _panel.style.height = oldHeight;
+
+    _updatePanel();
+  }
+
+  void _updatePanel() {
+    if (window.pageYOffset > _maxHeight - _minHeight) {
+      panelStyle = _COLLAPSED;
+      _panel.style.height = '';
+    } else if (window.pageYOffset > 0) {
+      _panel.style.height = '${_maxHeight - window.pageYOffset}px';
+    } else {
+      panelStyle = _EXPANDED;
+      _panel.style.height = '';
+    }
   }
 }
