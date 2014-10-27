d67cc39f8115b9551e6f60a5a1d813ba35f460d1
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-10-27 12:56:16 -0700

    Finally figured out how to make a failing scroll test

--- test/header_tests.dart
+++ test/header_tests.dart
@@ -4,16 +4,41 @@ class HeaderTests {
   static void run() {
     group('The intro header', () {
       test('element factory constructor works', introHeaderFactoryWorks);
-//      test('banner is present until the first card is reached',
-//           bannerIsPresentUntilFirstCardReached);
+
+      setUp(headerSetUp);
+      tearDown(headerTearDown);
+
+      test('banner is present until the first card is reached',
+           bannerIsPresentUntilFirstCardReached);
     });
+  }
+
+  static void headerSetUp() {
+    var el = new IntroHeaderElement();
+    el.id = 'header-test-el';
+    document.body.append(el);
+  }
+
+  static void headerTearDown() {
+    var el = document.getElementById('header-test-el');
+    el.remove();
   }
 
   static void introHeaderFactoryWorks() {
     expect(() => new IntroHeaderElement(), returnsNormally);
   }
-//
-//  static void bannerIsPresentUntilFirstCardReached() {
-//    var el = new Element.('intro-header');
-//  }
+
+  static Future bannerIsPresentUntilFirstCardReached() {
+    var extraHeight = new Element.div();
+    extraHeight.style.height = '5000px';
+    document.body.append(extraHeight);
+
+    return new Future(() => window.scroll(0, 600)).then((_) {
+      return new Future(() {
+        var shadowRoot = document.getElementById('header-test-el').shadowRoot;
+        var panel = shadowRoot.getElementById('panel');
+        expect(panel.classes, contains('panel-displayed'));
+      });
+    });
+  }
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,8 +1,9 @@
 library about_me_tests;
 
+import 'dart:async';
 import 'dart:html';
 import 'package:unittest/unittest.dart';
-import 'package:unittest/html_config.dart';
+import 'package:unittest/html_individual_config.dart';
 import 'package:about_me/header/intro_header.dart';
 import 'package:polymer/polymer.dart';
 
@@ -10,7 +11,7 @@ part 'header_tests.dart';
 
 void main() {
   initPolymer();
-  useHtmlConfiguration();
+  useHtmlIndividualConfiguration();
 
   HeaderTests.run();
 }
