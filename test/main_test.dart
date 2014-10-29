6def5f2032ef1c3ea5c181d346bd6003eb2601f3
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-10-29 14:25:09 -0700

    Karma is now up and running

--- test/main_test.dart
+++ test/main_test.dart
@@ -2,16 +2,20 @@ library about_me_tests;
 
 import 'dart:async';
 import 'dart:html';
+import 'package:angular/angular.dart';
+import 'package:angular/mock/module.dart';
 import 'package:unittest/unittest.dart';
-import 'package:unittest/html_individual_config.dart';
 import 'package:about_me/header/intro_header.dart';
-import 'package:polymer/polymer.dart';
+import '../web/main.dart';
 
 part 'header_tests.dart';
 
 void main() {
-  initPolymer();
-  useHtmlIndividualConfiguration();
-
+  setUp(() {
+    setUpInjector();
+    module((Module m) => m.install(new AboutMeNGApp()));
+  });
+  tearDown(tearDownInjector);
+  
   HeaderTests.run();
 }
