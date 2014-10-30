10df789b260c4bed36f796872dfe63083156ca7d
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-10-29 20:50:01 -0700

    Trying to get testing working but need to take a step back to square 1

--- test/header_tests.dart
+++ test/header_tests.dart
@@ -3,8 +3,6 @@ part of about_me_tests;
 class HeaderTests {
   static void run() {
     group('The intro header', () {
-      test('element factory constructor works', introHeaderFactoryWorks);
-
       setUp(headerSetUp);
       tearDown(headerTearDown);
 
@@ -17,46 +15,37 @@ class HeaderTests {
   }
 
   static void headerSetUp() {
-    var el = new IntroHeaderElement();
-    el.id = 'header-test-el';
-    document.body.append(el);
-
-    var extraHeight = new Element.div();
-    extraHeight.id = 'extra-height';
-    extraHeight.style.height = '5000px';
-    document.body.append(extraHeight);
-  }
-
-  static void headerTearDown() {
-    var el = document.getElementById('header-test-el');
-    el.remove();
+    module((Module m) => m.bind(Window, toValue: new MockWindow()));
     
-    var extraHeight = document.getElementById('extra-height');
-    extraHeight.remove();
+//    var extraHeight = new Element.div();
+//    extraHeight.id = 'extra-height';
+//    extraHeight.style.height = '5000px';
+//    document.body.append(extraHeight);
   }
 
-  static void introHeaderFactoryWorks() {
-    expect(() => new IntroHeaderElement(), returnsNormally);
+  static void headerTearDown() {    
+//    var extraHeight = document.getElementById('extra-height');
+//    extraHeight.remove();
   }
 
   static Future bannerIsPresentUntilFirstCardReached() {
-    return new Future(() => window.scroll(0, 500)).then((_) {
-      return new Future(() {
-        var shadowRoot = document.getElementById('header-test-el').shadowRoot;
-        var panel = shadowRoot.getElementById('panel');
-        expect(panel.classes, contains('panel-displayed'));
-      });
-    });
+    return async(inject((IntroHeaderElement header, ShadowRoot shadowRoot, Window win) {
+      header.onShadowRoot(shadowRoot);
+      win.scroll(0, 500);
+      
+      microLeap();
+      
+      expect(header.panelDisplayStyle, equals('panel-displayed'));
+    }));
   }
   
-  static Future bannerIsPresentWhenSlowlyScrolling() {
-    return new Future(() => window.scroll(0, 502)).then((_) {
-      return new Future.delayed(Duration.ZERO, () => window.scroll(0, 500));
-    }).then((_) {
-      return new Future.delayed(Duration.ZERO, () {
-        IntroHeaderElement header = document.getElementById('header-test-el');
-        expect(header.panelDisplayStyle, equals('panel-displayed'));
-      });
-    });
+  static void bannerIsPresentWhenSlowlyScrolling() {
+    async(inject((IntroHeaderElement header, Window win) {
+      win.scroll(0, 502);
+      microLeap();
+      win.scroll(0, 500);
+      microLeap();
+      expect(header.panelDisplayStyle, equals('panel-displayed'));
+    }));
   }
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular/mock/module.dart';
+//import 'package:polymer/polymer.dart';
 import 'package:unittest/unittest.dart';
 import 'package:about_me/header/intro_header.dart';
 import '../web/main.dart';
@@ -11,6 +12,7 @@ import '../web/main.dart';
 part 'header_tests.dart';
 
 void main() {
+//  initPolymer();
   setUp(() {
     setUpInjector();
     module((Module m) => m.install(new AboutMeNGApp()));
