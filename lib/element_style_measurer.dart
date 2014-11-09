6ea068c9cd5c14d7ca30f59b3ed721ddc7515811
Dylan Powers <dylan.kyle.powers@gmail.com>
2014-11-08 20:15:21 -0800

    Element style range evaluator refactored to the individual measurers

---
+++ lib/element_style_measurer.dart
@@ -0,0 +1,59 @@
+library element_style_range_evaluator;
+
+import 'dart:html';
+
+class TopStyleMeasurer extends ElStyleMeasurerBase {
+  String get oldStyleAttr => el.style.top;
+         set oldStyleAttr(String top) => el.style.top = top;
+  num _measure() => el.offsetTop;
+  
+  TopStyleMeasurer(Element el) : super(el);
+}
+
+class HeightStyleMeasurer extends ElStyleMeasurerBase {
+  String get oldStyleAttr => el.style.height;
+         set oldStyleAttr(String height) => el.style.height = height;
+  num _measure() => el.clientHeight;
+  
+  HeightStyleMeasurer(Element el) : super(el);
+}
+
+abstract class ElStyleMeasurerBase {
+  String get oldStyleAttr;
+         set oldStyleAttr(String);
+  num _measure();
+  
+  final Element el;
+  ElStyleMeasurerBase(this.el);
+  
+  Interval measureClassRange(String minClass, String maxClass) {
+    num min = measureClass(minClass, maxClass);
+    num max = measureClass(maxClass, minClass);
+    return new Interval(min, max);
+  }
+  
+  num measureClass(String className, String conflictingClass) {
+    var oldClasses = el.className;
+    var oldElStyle = oldStyleAttr;
+    var oldTransitionDuration = el.style.transitionDuration;
+
+    oldStyleAttr = '';
+    el.style.transitionDuration = '0';
+    el.classes.remove(conflictingClass);
+    el.classes.add(className);
+    num measurement = _measure();
+
+    el.className = oldClasses;
+    el.style.transitionDuration = oldTransitionDuration;
+    oldStyleAttr = oldElStyle;
+
+    return measurement;
+  }
+}
+
+class Interval {
+  num min = 0;
+  num max = 0;
+  num get range => max - min;
+  Interval(this.min, this.max);
+}
--- test/element_style_measurer_tests.dart
+++ test/element_style_measurer_tests.dart
@@ -6,36 +6,67 @@ void runElementStyleRangeEvaluatorTests() {
 }
 
 class _ElementStyleRangeEvaluatorTests {
-  ElementStyleRangeEvaluator _testEvaluator;
+  ElStyleMeasurerBase _testEvaluator;
   StyleElement _style;
   _ElementStyleRangeEvaluatorTests() {
-    beforeEach(() => _testEvaluator = new ElementStyleRangeEvaluator(new Element.div()));
     describe('top range method', () {
+      beforeEach(() => _testEvaluator = new TopStyleMeasurer(new Element.div()));
       it('should exist', () =>
-         expect(() => _testEvaluator.evalTop('foo', 'bar')).not.toThrow());
+         expect(() => _testEvaluator.measureClassRange('foo', 'bar')).not.toThrow());
       it('should return an interval of 0 for non-existant css classes', () =>
-         expect(_testEvaluator.evalTop('foo', 'bar').range).toEqual(0));
+         expect(_testEvaluator.measureClassRange('foo', 'bar').range).toEqual(0));
 
-      beforeEach(_beforeEach);
+      beforeEach(() {
+        var div = new Element.div();
+        div.id = 'style-range-test-div';
+        div.style.position = 'absolute';
+        document.body.append(div);
+        _testEvaluator = new TopStyleMeasurer(div);
+
+        _style = new StyleElement();
+        document.body.append(_style);
+      });
+
       afterEach(_afterEach);
+
       it('should return a min/max value of 1 for a min/max top value of 1',
          _minTopIs1);
     });
 
     describe('height range method', () {
+      beforeEach(() => _testEvaluator = new HeightStyleMeasurer(new Element.div()));
       it('should exist', () => 
-         expect(() => _testEvaluator.evalHeight('foo', 'bar')).not.toThrow());
+         expect(() => _testEvaluator.measureClassRange('foo', 'bar')).not.toThrow());
       it('should return an interval of 0 for non-existant css classes', () =>
-         expect(_testEvaluator.evalHeight('foo', 'bar').range).toEqual(0));
+         expect(_testEvaluator.measureClassRange('foo', 'bar').range).toEqual(0));
       
-      beforeEach(_beforeEach);
+      beforeEach(() {
+        var div = new Element.div();
+        div.id = 'style-range-test-div';
+        div.style.position = 'absolute';
+        document.body.append(div);
+        _testEvaluator = new HeightStyleMeasurer(div);
+
+        _style = new StyleElement();
+        document.body.append(_style);
+      });
+
       afterEach(_afterEach);
       it('should return a min/max value of 1 for a min/max top value of 1',
          _minHeightIs1);
       it('should return a range of 1 for min of 0 and max of 1', _rangeHeightIs1);
     });
     
-    beforeEach(_beforeEach);
+    beforeEach(() {
+      var div = new Element.div();
+      div.id = 'style-range-test-div';
+      div.style.position = 'absolute';
+      document.body.append(div);
+      _testEvaluator = new HeightStyleMeasurer(div);
+
+      _style = new StyleElement();
+      document.body.append(_style);
+    });
     afterEach(_afterEach);
     
     it('should handle elements with transitions', () {
@@ -45,17 +76,6 @@ class _ElementStyleRangeEvaluatorTests {
     });
   }
 
-  void _beforeEach() {
-    var div = new Element.div();
-    div.id = 'style-range-test-div';
-    div.style.position = 'absolute';
-    document.body.append(div);
-    _testEvaluator = new ElementStyleRangeEvaluator(div);
-
-    _style = new StyleElement();
-    document.body.append(_style);
-  }
-
   void _afterEach() {
     document.getElementById('style-range-test-div').remove();
     _style.remove();
@@ -63,25 +83,25 @@ class _ElementStyleRangeEvaluatorTests {
 
   void _minTopIs1() {
     _style.appendText('.foo { top: 1px } .bar { top: 1px }');
-    var inter = _testEvaluator.evalTop('foo', 'bar');
+    var inter = _testEvaluator.measureClassRange('foo', 'bar');
     expect(inter.min).toEqual(1);
     expect(inter.max).toEqual(1);
   }
 
   void _rangeTopIs1() {
     _style.appendText('.foo { top: 0px } .bar { top: 1px }');
-    expect(_testEvaluator.evalTop('foo', 'bar').range).toEqual(1);
+    expect(_testEvaluator.measureClassRange('foo', 'bar').range).toEqual(1);
   }
 
   void _minHeightIs1() {
     _style.appendText('.foo { height: 1px } .bar { height: 1px }');
-    var inter = _testEvaluator.evalHeight('foo', 'bar');
+    var inter = _testEvaluator.measureClassRange('foo', 'bar');
     expect(inter.min).toEqual(1);
     expect(inter.max).toEqual(1);
   }
 
   void _rangeHeightIs1() {
     _style.appendText('.foo { height: 0px } .bar { height: 1px }');
-    expect(_testEvaluator.evalHeight('foo', 'bar').range).toEqual(1);
+    expect(_testEvaluator.measureClassRange('foo', 'bar').range).toEqual(1);
   }
 }
