3df87cfc7ef1d4e86e9c88e975d301617f9867ca
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-01-06 11:45:52 -0800

    The overflowed links menu should hide when told to do so
    The final test is to implement it and see if it works as expected

--- lib/header/overflowed_links_menu.dart
+++ lib/header/overflowed_links_menu.dart
@@ -3,14 +3,39 @@ part of intro_header;
 const String OVERFLOWED_LINKS_MENU_TAG = 'overflowed-links-menu';
 @CustomTag(OVERFLOWED_LINKS_MENU_TAG)
 class OverflowedLinksMenuElement extends PolymerElement {
+  static const OPEN_DROPDOWN_CLASSNAME = 'core-opened';
+
   Rectangle get buttonDimensions =>
       shadowRoot.getElementById('links-menu-button').getBoundingClientRect();
-  @published bool isMenuOpen = false;
+
+  @published bool get isMenuOpen => readValue(#isMenuOpen, () => false);
+                  set isMenuOpen(bool value) {
+    if (_linksDropdown != null) {
+      if (value) {
+        _linksDropdown.classes.add(OPEN_DROPDOWN_CLASSNAME);
+      } else {
+        _linksDropdown.classes.remove(OPEN_DROPDOWN_CLASSNAME);
+      }
+    }
+
+    writeValue(#isMenuOpen, value);
+  }
+
   @published List overflowedLinks = new List<OverflowedHeaderLink>();
+
+  HtmlElement _linksDropdown;
 
   factory OverflowedLinksMenuElement() {
     return new Element.tag(OVERFLOWED_LINKS_MENU_TAG);
   }
 
   OverflowedLinksMenuElement.created() : super.created();
+
+  @override
+  void attached() {
+    _linksDropdown = shadowRoot.getElementById('links-dropdown');
+    if (isMenuOpen) {
+      _linksDropdown.classes.add(OPEN_DROPDOWN_CLASSNAME);
+    }
+  }
 }
--- test/header/overflowed_links_menu_tests.dart
+++ test/header/overflowed_links_menu_tests.dart
@@ -7,26 +7,51 @@ import 'package:unittest/unittest.dart' as ut;
 import 'package:about_me/header/header.dart';
 
 void run() {
-  describe('The overflowed links menu', () {
-    OverflowedLinksMenuElement overLinksMenu;
-    beforeEach(() => overLinksMenu = new OverflowedLinksMenuElement());
+  ddescribe('The overflowed links menu', () {
+    beforeEach(() {
+      _overLinksMenu = new OverflowedLinksMenuElement();
+      document.body.append(_overLinksMenu);
+    });
+    afterEach(() => _overLinksMenu.remove());
 
     it('has a working element factory constructor', () =>
         expect(() => new OverflowedLinksMenuElement()).not.toThrow());
 
     it('has an attribute that holds a list of OverflowedLinks', () {
-      expect(overLinksMenu.overflowedLinks,
+      expect(_overLinksMenu.overflowedLinks,
              new ut.isInstanceOf<List<OverflowedHeaderLink>>());
     });
 
     it('has an attribute that describes whether or not the menu should be open', () {
-      expect(() => overLinksMenu.isMenuOpen).not.toThrow();
-      expect(overLinksMenu.isMenuOpen, new ut.isInstanceOf<bool>());
+      expect(() => _overLinksMenu.isMenuOpen).not.toThrow();
+      expect(_overLinksMenu.isMenuOpen, new ut.isInstanceOf<bool>());
     });
 
     it('has a property for accessing the menu button dimensions', () {
-      expect(() => overLinksMenu.buttonDimensions).not.toThrow();
-      expect(overLinksMenu.buttonDimensions, new ut.isInstanceOf<Rectangle>());
+      expect(() => _overLinksMenu.buttonDimensions).not.toThrow();
+      expect(_overLinksMenu.buttonDimensions, new ut.isInstanceOf<Rectangle>());
+    });
+
+    it('when set to open is open', _testOpen);
+
+    it('when set to closed is closed', () {
+      var dropdown = _testOpen();
+      _overLinksMenu.isMenuOpen = false;
+      expect(dropdown.classes
+                     .contains(OverflowedLinksMenuElement.OPEN_DROPDOWN_CLASSNAME))
+          .toBeFalse();
     });
   });
+}
+
+
+OverflowedLinksMenuElement _overLinksMenu;
+
+HtmlElement _testOpen() {
+  _overLinksMenu.isMenuOpen = true;
+  var dropdown = _overLinksMenu.shadowRoot.getElementById('links-dropdown');
+  expect(dropdown.classes
+                 .contains(OverflowedLinksMenuElement.OPEN_DROPDOWN_CLASSNAME))
+      .toBeTrue();
+  return dropdown;
 }
