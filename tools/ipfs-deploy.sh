171b094a0842f9df69bdcdf34cc3c3f62d45ce92
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-03-29 13:41:34 -0700

    deploy: Figured out to use ssh -t as a workaround to an ipfs bug
    https://github.com/jbenet/go-ipfs/issues/981

--- tools/ipfs-deploy.sh
+++ tools/ipfs-deploy.sh
@@ -3,19 +3,20 @@
 build_tarball=build/web.tar
 server_addr=home.dylankpowers.com
 ssh_port=48924
+ssh="ssh -p $ssh_port $server_addr"
 
 echo "Publishing to IPNS"
 
 tar -cf $build_tarball -C build web
 
 temp_dir=/tmp/dkp-build.$RANDOM
-ssh -p $ssh_port $server_addr "mkdir --parents $temp_dir"
+$ssh "mkdir --parents $temp_dir"
 scp -P $ssh_port $build_tarball $server_addr:$temp_dir/web.tar
 
 printf "\nAdding files to IPFS...this may take awhile. Go take a snack break\n"
-ssh -p $ssh_port $server_addr \
+$ssh -t \
   "tar -xf web.tar -C $temp_dir && \
-   ipfs add --recursive --quiet $temp_dir/web > $temp_dir/hashes && \
+   ipfs add --recursive --quiet $temp_dir/web | tee $temp_dir/hashes && \
    tail -n 1 $temp_dir/hashes | ipfs name publish"
 
 if [ $? -ne 0 ]; then
@@ -23,3 +24,5 @@ if [ $? -ne 0 ]; then
 else
   echo "IPFS publish succeeded!"
 fi
+
+$ssh "rm -r $temp_dir"
