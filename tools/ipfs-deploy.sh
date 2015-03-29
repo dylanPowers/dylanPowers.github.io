72c65c6f0f03401c84764989cbfe20884f40d8f5
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-03-29 14:47:35 -0700

    deploy: Finally found my issue with the tarball. I was targetting the wrong archive

--- tools/ipfs-deploy.sh
+++ tools/ipfs-deploy.sh
@@ -7,7 +7,6 @@ ssh="ssh -p $ssh_port $server_addr"
 
 echo "Publishing to IPNS"
 
-rm $build_tarball &>/dev/null
 tar --create --file=$build_tarball --directory=build web
 
 temp_dir=/tmp/dkp-build.$RANDOM
@@ -16,7 +15,7 @@ scp -P $ssh_port $build_tarball $server_addr:$temp_dir/web.tar
 
 printf "\nAdding files to IPFS...this may take awhile. Go take a snack break\n"
 $ssh -t \
-  "tar -xf web.tar -C $temp_dir && \
+  "tar -xf $temp_dir/web.tar -C $temp_dir && \
    ipfs add --recursive --quiet $temp_dir/web | tee $temp_dir/hashes && \
    tail -n 1 $temp_dir/hashes | ipfs name publish"
 
