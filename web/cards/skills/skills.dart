383ee7b3b04f6f2f5a324571af666d37d528a8eb
Dylan Powers <dylan.kyle.powers@gmail.com>
2015-12-27 20:23:20 -0800

    Skillz charts updated

--- web/cards/skills/skills.dart
+++ web/cards/skills/skills.dart
@@ -8,6 +8,7 @@ const String TAG_NAME = 'dkp-skills';
 class SkillsElement extends PolymerElement {
   static const LANGS_CHART_ID = 'languages-chart';
   static const PLATFORMS_CHART_ID = 'platforms-chart';
+  static const TOOLS_CHART_ID = 'tools-chart';
 
   factory SkillsElement() {
     return new Element.tag(TAG_NAME);
@@ -27,23 +28,24 @@ class SkillsElement extends PolymerElement {
   }
 
   void _renderCharts() {
-    _renderLangsChart();
-    _renderPlatformsChart();
-  }
-
-  void _renderLangsChart() {
     new LangsChart().renderTo(shadowRoot, LANGS_CHART_ID);
-  }
-
-  void _renderPlatformsChart() {
     new PlatformsChart().renderTo(shadowRoot, PLATFORMS_CHART_ID);
+    new ToolsChart().renderTo(shadowRoot, TOOLS_CHART_ID);
   }
+}
+
+class Skill {
+  final String name;
+  final int relativeExp;
+  final double years;
+
+  const Skill(this.name, this.relativeExp, this.years);
 }
 
 abstract class SkillsChart {
   Map _chartOptions = {
     'chart': {
-      'marginLeft': 64,
+      'marginLeft': 80,
       'type': 'bar'
     },
     'colors': ['#FFAB00', '#D50000'],
@@ -82,16 +84,18 @@ abstract class SkillsChart {
     }
   };
 
-  SkillsChart(List<String> categories, List<int> relativeExp, List<int> years,
+  final List<Skill> skills;
+
+  SkillsChart(this.skills,
               {Map optionOverrides: const {}}) {
     _chartOptions.addAll({
-      'xAxis': { 'categories': categories },
+      'xAxis': { 'categories': skills.map((skill) => skill.name) },
       'series': [{
         'name': 'Years',
-        'data': years
+        'data': skills.map((skill) => skill.years)
       }, {
         'name': 'Relative Knowledge',
-        'data': relativeExp
+        'data': skills.map((skill) => skill.relativeExp)
       }]
     });
 
@@ -100,6 +104,7 @@ abstract class SkillsChart {
 
   void renderTo(ShadowRoot shadowRoot, String elementId) {
     var el = shadowRoot.getElementById(elementId);
+    el.style.setProperty('height', '${48 * skills.length + 90}px');
     _chartOptions['chart']['renderTo'] = el;
     var jsChartOptions = new js.JsObject.jsify(_chartOptions);
     new js.JsObject(js.context['Highcharts']['Chart'], [jsChartOptions]);
@@ -107,33 +112,43 @@ abstract class SkillsChart {
 }
 
 class LangsChart extends SkillsChart {
-  static const _CATEGORIES = const [
-    'Dart', 'C', 'C++', 'JS', 'Java', 'C#', 'Go'
+  static const _SKILLS = const [
+    const Skill('Dart', 5, 3.0),
+    const Skill('C', 5, 4.0),
+    const Skill('CSS', 5, 3.5),
+    const Skill('C++', 3, 3.0),
+    const Skill('Bash', 3, 2.0),
+    const Skill('Java', 3, 2.0),
+    const Skill('C#', 3, 1.0),
+    const Skill('JS', 2, 2.0),
+    const Skill('Go', 2, 0.5),
+    const Skill('Ruby', 1, 1.0),
+    const Skill('PHP', 1, 1.0)
   ];
 
-  static const _RELATIVE_EXP = const [
-    5, 4, 3, 3, 3, 3, 1
-  ];
-
-  static const _YEARS = const [
-    3, 4, 3, 3, 2, 2, 1
-  ];
-
-  LangsChart() : super(_CATEGORIES, _RELATIVE_EXP, _YEARS);
+  LangsChart() : super(_SKILLS);
 }
 
 class PlatformsChart extends SkillsChart {
-  static const _CATEGORIES = const [
-    'Web', 'Android'
+  static const _SKILLS = const [
+    const Skill('Android SDK', 4, 2.0),
+    const Skill('Android NDK', 4, 1.0),
+    const Skill('Polymer.dart', 3, 2.0),
+    const Skill('Ruby on Rails', 2, 2.0),
+    const Skill('Chrome App/Ext', 2, 1.0),
+    const Skill('OpenGL', 1, 1.0)
   ];
 
-  static const _RELATIVE_EXP = const [
-    5, 3
+  PlatformsChart() : super(_SKILLS);
+}
+
+class ToolsChart extends SkillsChart {
+  static const _SKILLS = const [
+    const Skill('Git', 5, 4.0),
+    const Skill('Linux', 5, 4.0),
+    const Skill('Unit Testing', 3, 2.0),
+    const Skill('Docker', 2, 0.5),
   ];
 
-  static const _YEARS = const [
-    3, 2
-  ];
-
-  PlatformsChart() : super(_CATEGORIES, _RELATIVE_EXP, _YEARS);
+  ToolsChart() : super(_SKILLS);
 }
